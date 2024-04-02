65cbe630a1388897361a20700c428b6f0974a5d5
Martin <martinisaiasbrandalisijimenez@gmail.com>
2024-04-01 23:41:53 -0300

    archivo modificado

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo" << endl;
-
+    int *p, a;
+    
+    p=&a;
+    
+    cout << "Contenido del puntero: " << *p << endl;
+    cout << "Direccion de memoria almacenada por el puntero: " << p << endl;
+    cout << "Direccion de memoria de la variable: " << &a << endl;
+    cout << "Direccion de memoria del puntero: " << &p << endl;
+    cout << "Tamaño de memoria utilizada por la variable: " << sizeof(a) << endl;
+    
     return 0;
 }
